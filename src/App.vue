c4ac51a3739d3bb2592b2e18441af2695f1ea0bc
Matt <mattsocha11@gmail.com>
2022-09-15 22:24:46 -0400

    vue project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <ShortAnswer :aqilioData="{component_data: {title: 'title here123', showBackButton: true}}"/>
+
+</template>
+
+
+<script>
+
+import ShortAnswer from './components/ShortAnswer.vue'
+
+export default {
+  name: 'App',
+  props: [],
+  components: {
+    ShortAnswer
+  },
+  data() {
+    return {
+    }
+  },
+  mounted() {
+    console.log("userTextInput mount");
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ShortAnswer.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="parent">
+    <div class="child">
+      <label v-if="!aqilioData" for="shortAnswer">component_data not found</label>
+
+      <label v-if="aqilioData" for="shortAnswer"> {{ aqilioData.component_data.title }} </label>
+      <input type="text" id="shortAnswer" name="shortAnswer"><br><br>
+
+
+      <!-- think of logic on when to show back button -->
+      <button v-if="aqilioData && aqilioData.component_data.showBackButton" class="button back-btn" style="margin-right: 15px">Back</button>
+      <button class="button">Next</button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'ShortAnswer',
+  props: ['aqilioData'],
+  components: {},
+  data() {
+    return {
+      title: '',
+      value: null,
+    }
+  },
+  mounted() {
+    console.log("ShortAnswer");
+    if (this.aqilioData) {
+      this.title = this.aqilioData.value
+    }
+  },
+  methods: {
+    goNext() {
+        this.$aqilio.setCurrentStepValue(this.value)
+        this.$aqilio.nextStep()
+    },
+    goBack() {
+        this.$aqilio.previousStep()
+    }
+  }
+};
+</script>
+
+<style scoped>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+div {
+  font-family: Verdana,sans-serif;
+  font-size: 15px;
+  border-radius: 5px;
+  padding: 20px;
+}
+
+.parent {
+  height: 200px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.button {
+  background-color: #4f46e5;
+  border-radius: 0.375rem;
+  border: none;
+  color: white;
+  padding: 11px 40px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+
+.back-btn {
+  color: #94a3b8;
+  background-color: white;
+  border: 1px solid #94a3b8;
+}
+</style>
